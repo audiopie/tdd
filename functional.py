1594de9894f9b8f570b827d6d23025d06b1920eb
audiopie <surroundyou@ya.ru>
2019-10-18 17:17:02 +0300

    HTML page generate from templates

--- functional.py
+++ functional.py
@@ -18,18 +18,18 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_elements_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
         'Enter a to-do item'
         )
-        input.send_keys('Купить павлинья перья')
+        inputbox.send_keys('Купить павлинья перья')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_name('tr')
+        rows = table.find_elements_by_name('tr')
         self.assertTrue(
-            any(row.text == '1: Купить павлинья перья' for row in rows))
+            any(row.text == '1: Купить павлинья перья' for row in rows), "Новый элемент списка не появился в таблице")
         
 
         self.fail('Закончить тест')
--- lists/tests.py
+++ lists/tests.py
@@ -9,21 +9,25 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
-        ''' Тест:  корневой url  преобразуется в представление домашней страницы  '''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-    
-    def test_home_page_returns_correct_html(self):
-        ''' тест:  домашняя страница возвращает правильный html  '''
+    def test_uses_home_template(self):
         response = self.client.get('/')
-        html = response.content.decode('utf8')
-        # expected_html = render_to_string('home.html')
-        # self.assertEqual(html, expected_html)
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-
-        self.assertTrue(html.strip().endswith('</html>'))
-
         self.assertTemplateUsed(response, 'home.html')
+
+    # def test_root_url_resolve_to_home_page_view(self):
+    #     ''' Тест:  корневой url  преобразуется в представление домашней страницы  '''
+    #     found = resolve('/')
+    #     self.assertEqual(found.func, home_page)
+    
+    # def test_home_page_returns_correct_html(self):
+    #     ''' тест:  домашняя страница возвращает правильный html  '''
+    #     response = self.client.get('/')
+    #     html = response.content.decode('utf8')
+    #     # expected_html = render_to_string('home.html')
+    #     # self.assertEqual(html, expected_html)
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.endswith('</html>'))
+
+    #     self.assertTrue(html.strip().endswith('</html>'))
+
+    #     self.assertTemplateUsed(response, 'home.html')
