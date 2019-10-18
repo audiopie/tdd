7f8d2e4882c95907ae21113c1e4f08fcb60518eb
audiopie <surroundyou@ya.ru>
2019-10-18 16:22:09 +0300

    Ft check to input element of lists

--- functional.py
+++ functional.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,7 +15,24 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://127.0.0.1:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончили!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_elements_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+        'Enter a to-do item'
+        )
+        input.send_keys('Купить павлинья перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлинья перья' for row in rows))
+        
+
+        self.fail('Закончить тест')
 
 if __name__ == '__main__':
     unittest.main()
