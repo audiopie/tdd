6148b1b73043a3389e6f9d2a9b8f31b4c047d5e5
audiopie <surroundyou@ya.ru>
2019-10-18 15:55:12 +0300

    Basic view return minimal HTML

--- functional.py
+++ functional.py
@@ -10,10 +10,10 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://127.0.0.1:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish test')
+        self.fail('Закончили!')
 
 if __name__ == '__main__':
     unittest.main()
