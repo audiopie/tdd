8b2c0ca385dfe0220cd58b60d9ff802a1e7239ba
audiopie <surroundyou@ya.ru>
2020-07-08 23:23:59 +0300

    chapter_post_and_database

--- functional.py
+++ functional.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         """ Установка """
@@ -10,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://127.0.0.1:8000')
@@ -19,21 +25,26 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'),
-        'Enter a to-do item'
-        )
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 
+            'Enter a to-do item'
+            )
         inputbox.send_keys('Купить павлинья перья')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлинья перья' for row in rows), "Новый элемент списка не появился в таблице")
-        
+        self.check_for_row_in_list_table('1: Купить павлинья перья')
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        self.check_for_row_in_list_table('1: Купить павлинья перья')
+        
         self.fail('Закончить тест')
 
-if __name__ == '__main__':
-    unittest.main()
 
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -4,30 +4,65 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
+
 
 
 
 class HomePageTest(TestCase):
 
+    def test_only_saves_item_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    # def test_root_url_resolve_to_home_page_view(self):
-    #     ''' Тест:  корневой url  преобразуется в представление домашней страницы  '''
-    #     found = resolve('/')
-    #     self.assertEqual(found.func, home_page)
-    
-    # def test_home_page_returns_correct_html(self):
-    #     ''' тест:  домашняя страница возвращает правильный html  '''
-    #     response = self.client.get('/')
-    #     html = response.content.decode('utf8')
-    #     # expected_html = render_to_string('home.html')
-    #     # self.assertEqual(html, expected_html)
-    #     self.assertTrue(html.startswith('<html>'))
-    #     self.assertIn('<title>To-Do lists</title>', html)
-    #     self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        '''тест используется домашний шаблон'''
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    #     self.assertTrue(html.strip().endswith('</html>'))
+    def test_can_save_a_POST_request(self):
+        '''сохраняет post запрос'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
 
-    #     self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='iemey 1')
+        Item.objects.create(text='iemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('iemey 1', response.content.decode())
+        self.assertIn('iemey 2', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
